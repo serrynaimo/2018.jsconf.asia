fb1ebea8fa560b2d45964b2f20566ae7365d4cbe
Thomas Gorissen <mail@serrynaimo.com>
2017-10-23 15:17:05 +0800

    prod env changed

--- http2server.production.conf.js
+++ http2server.production.conf.js
@@ -3,7 +3,7 @@ const manifest = require('./http2server.manifest')
 module.exports = {
   hosts: [
     {
-      domain: '2018-jsconf-asia.http2.live',
+      domain: '2018.jsconf.asia',
       manifest
     }
   ]
